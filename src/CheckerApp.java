89b74d9461cc14bc988b5ecd212dd0d991cefadc
ik7854-cmyk <ik7854@srmist.edu.in>
2026-02-25 11:26:56 +0530

    UC6

--- src/CheckerApp.java
+++ src/CheckerApp.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class CheckerApp {
 
@@ -7,22 +9,24 @@ public class CheckerApp {
         System.out.println("=================================================");
         System.out.println("        WELCOME TO PALINDROME CHECKER");
         System.out.println("=================================================");
-        System.out.println("This application checks whether a word is a palindrome.");
-        System.out.println("Using Stack (LIFO) Technique");
+        System.out.println("Demonstrating FIFO (Queue) vs LIFO (Stack)");
         System.out.println("=================================================");
 
         String word = "madam";
 
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
         }
 
         boolean isPalindrome = true;
 
         for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != stack.pop()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
