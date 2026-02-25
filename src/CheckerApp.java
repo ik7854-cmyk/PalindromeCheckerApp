0e8bd2c531a5d70f4983090fb12ed8190c09150a
ik7854-cmyk <ik7854@srmist.edu.in>
2026-02-25 09:42:35 +0530

    UC2:Hardcoded palindrome

--- src/CheckerApp.java
+++ src/CheckerApp.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
-public class CheckerApp
-    {
-        public static void main(String args[])
-            {
-                System.out.println("=================================================");
-                System.out.println(" WELCOME TO PALINDROME CHECKER");
-                System.out.println("=================================================");
-                System.out.println("This application checks whether a word is a palindrome.");
-                System.out.println("Stay tumed for upcoming features!!!");
-             }
+public class CheckerApp {
+    public static void main(String args[]) {
+        System.out.println("=================================================");
+        System.out.println(" WELCOME TO PALINDROME CHECKER");
+        System.out.println("=================================================");
+        System.out.println("This application checks whether a word is a palindrome.");
+        System.out.println("Stay tuned for upcoming features!!!");
+        String word = "madam";
+        String reversed = " ";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+        System.out.println("\nCheckiing word : " + word);
+        if (word.equals(reversed)) {
+            System.out.println("Result : It is a Palindrome");
+        } else {
+            System.out.println("Result : It is not a Palindrome");
+        }
     }
+}
