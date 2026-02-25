063141fbf85b253102ec6695fa5ed7fb1f4adf5f
ik7854-cmyk <ik7854@srmist.edu.in>
2026-02-25 09:25:43 +0530

    UC1:Add welcome message module

--- src/CheckerApp.java
+++ src/CheckerApp.java
@@ -1,2 +1,12 @@
-public class CheckerApp {
-}
+import java.util.Scanner;
+public class CheckerApp
+    {
+        public static void main(String args[])
+            {
+                System.out.println("=================================================");
+                System.out.println(" WELCOME TO PALINDROME CHECKER");
+                System.out.println("=================================================");
+                System.out.println("This application checks whether a word is a palindrome.");
+                System.out.println("Stay tumed for upcoming features!!!");
+             }
+    }
