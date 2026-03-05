eb68032063bcadb3ac49c3622fed0a92474c3657
ik7854-cmyk <ik7854@srmist.edu.in>
2026-03-05 10:05:01 +0530

    UC8 Linked List

--- src/CheckerApp.java
+++ src/CheckerApp.java
@@ -1,45 +1,92 @@
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class CheckerApp {
 
-    public static void main(String args[]) {
+    public static Node createList(String word) {
+        Node head = null, tail = null;
 
-        System.out.println("=================================================");
-        System.out.println("        WELCOME TO PALINDROME CHECKER");
-        System.out.println("=================================================");
-        System.out.println("Demonstrating FIFO (Queue) vs LIFO (Stack)");
-        System.out.println("=================================================");
+        for (char c : word.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Find middle using fast & slow pointers
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+        // Compare both halves
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("WELCOME");
+        System.out.println("====================================");
+        System.out.println("Palindrome Checker - LinkedList Version");
+        System.out.println("====================================");
 
         String word = "madam";
 
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
+        Node head = createList(word);
 
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            stack.push(ch);
-            queue.add(ch);
-        }
-
-        boolean isPalindrome = true;
-
-        for (int i = 0; i < word.length(); i++) {
-            if (!queue.remove().equals(stack.pop())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        System.out.println("\nChecking word : " + word);
-
-        if (isPalindrome) {
-            System.out.println("Result : It is a Palindrome");
+        if (isPalindrome(head)) {
+            System.out.println(word + " is a Palindrome.");
         } else {
-            System.out.println("Result : It is not a Palindrome");
+            System.out.println(word + " is NOT a Palindrome.");
         }
-
-        System.out.println("=================================================");
     }
 }
